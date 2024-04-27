ff257434c010b16a17fa5528f1b1a0e8ded91903
Rishabh Sankla <rishabhsankla1@gmail.com>
2024-04-27 23:57:19 +0530

    Bugfix and changed readline to _c_getline

--- lineio.h
+++ lineio.h
@@ -1,11 +1,17 @@
-#ifndef READLINE_H
-#define READLINE_H
+/*
+ * lineio.h
+ *
+ * Declarations for Windows-equivalent
+ * implementations of POSIX I/O functions.
+*/
+#ifndef _LINEIO_H
+#define _LINEIO_H
 
 #define ALLOCATION_FAILURE -1
 #define EMPTY_READ 0
 
 #include <stdio.h>
 
-size_t readline(char **, FILE *);
+size_t _c_getline(char **, size_t *, FILE *);
 
 #endif
