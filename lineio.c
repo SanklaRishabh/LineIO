2c865f9b090579e271f4760b2512301e9856b516
Rishabh Sankla <rishabhsankla1@gmail.com>
2024-05-01 00:21:30 +0530

    Increased time and memory efficiency

--- lineio.c
+++ lineio.c
@@ -1,41 +1,82 @@
 #include "lineio.h"
+#include <errno.h>
+#include <stdbool.h>
 #include <stdlib.h>
 
-size_t readline(char **buffer, FILE *ptr) {
-  *buffer = NULL;
-  size_t bufSize = 0;
-  char charBuf;
-
-  while ((charBuf = fgetc(ptr)) != EOF) {
-    *buffer = realloc(*buffer, bufSize + 1);
-
-    if (*buffer == NULL) {
-      perror("realloc()");
-
-      return ALLOCATION_FAILURE;
+size_t _c_getline(char **buf, size_t *bufSize, FILE *stream) {
+    // Check whether the eof indicator is set.
+    if (feof(stream)) {
+        return READ_ERROR;
     }
 
-    (*buffer)[bufSize++] = charBuf;
-
-    if (charBuf == '\n') {
-      break;
+    // Check parameters.
+    // `buf` and `bufSize` cannot be NULL.
+    if (buf == NULL || bufSize == NULL) {
+        return errno = EINVAL;
     }
-  }
 
-  if (bufSize == 0 && charBuf == EOF) {
-    return EMPTY_READ;
-  }
+    if (*bufSize == 0) {
+        // Default size of the buffer.
+        *bufSize = 120;
+        *buf = (char *)malloc(*bufSize * sizeof(char));
 
-  *buffer = realloc(*buffer, bufSize + 1);
+        // Failure in allocating memory.
+        if (*buf == NULL) {
+            perror("realloc");
 
-  if (*buffer == NULL) {
-    perror("realloc()");
+            return errno = ENOMEM;
+        }
+    }
 
-    return ALLOCATION_FAILURE;
-  }
+    bool eofCheck;
+    char charBuf;
+    char *failsafe;
+    long startPos, endPos;
+    startPos = ftell(stream);
+    eofCheck = false;
 
-  (*buffer)[bufSize] = '\0';
+    while ((charBuf = fgetc(stream))) {
+        if (charBuf == '\n' || charBuf == EOF) {
+            endPos = ftell(stream);
+            size_t posDif = (size_t)(endPos - startPos);
+            failsafe = *buf;
 
-  // Line reading successful.
-  return (size_t)bufSize;
+            // Reallocate new size to the buffer.
+            if (posDif >= 2 * *bufSize) {
+                *buf = (char *)realloc(*buf, (*bufSize = posDif + 1));
+            } else if (posDif >= *bufSize) {
+                *buf = (char *)realloc(*buf, (*bufSize *= 2));
+            }
+
+            // Failure in allocating memory.
+            if (*buf == NULL) {
+                perror("realloc");
+
+                *buf = failsafe;
+
+                return errno = ENOMEM;
+            }
+
+            if (charBuf == EOF) {
+                eofCheck = true;
+            }
+
+            // Store the line in the buffer.
+            // CAUTION: Seeking resets eof indicator.
+            fseek(stream, startPos, SEEK_SET);
+            fgets(*buf, *bufSize, stream);
+            fseek(stream, endPos, SEEK_SET);
+
+            break;
+        }
+    }
+
+    if (eofCheck) {
+        // Set the eof indicator again.
+        fgetc(stream);
+        fgetc(stream);
+    }
+
+    // Successful line read.
+    return *bufSize;
 }
